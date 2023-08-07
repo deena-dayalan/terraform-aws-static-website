0d5b663d0345fecd3f95b8890b03601dd054869c
Deena <deena@TABLET-OOOM1I6T>
2023-08-07 18:44:14 -0400

    Adding bucket endpoint info in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "bucket-name-arn" {
   value       = aws_s3_bucket.static-website-bucket.arn
 }
 
+output "bucket-endpoint" {
+  description = "S3 bucket endpoint"
+  value       = aws_s3_bucket_website_configuration.static-website-bucket.website_endpoint
+}
+
 output "static-website-url" {
   description = "HTTP url"
   value       = "http://${aws_s3_bucket_website_configuration.static-website-bucket.website_endpoint}"
